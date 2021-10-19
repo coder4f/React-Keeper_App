062df861c53e913891bce373d2db895732550d85
Ramadan <ramco95@abv.bg>
2021-10-19 11:00:59 +0300

    Keeper App v2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(object => {
+        return (
+          <Note 
+          key={object.key}
+          title={object.title}
+          content={object.content}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
